f8d271b9eb96c8e204a24521cbeae162aa958c28
MatheusPatriota <mpatriota55@gmail.com>
2021-10-23 20:08:01 -0300

    user interface and access interface created

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -8,18 +8,39 @@ import axios from "axios";
  * Retornar token com infos dos user logado
  */
 
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse{
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
 
-    const response = await axios.post(url, null, {
-      params: {
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
-        client_id: process.env.GITHUB_CLIENT_ID,
-        code,
-      },
+    const { data: accessTokenReponse } = await axios.post<IAccessTokenResponse>(
+      url,
+      null,
+      {
+        params: {
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          client_id: process.env.GITHUB_CLIENT_ID,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
       headers: {
-        Accept: "application/json",
+        authorization: `Bearer ${accessTokenReponse.access_token}`,
       },
     });
 
