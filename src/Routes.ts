d6ccbe50aee46c5d345985646c2991f5b86d22dc
MatheusPatriota <mpatriota55@gmail.com>
2021-10-23 21:56:57 -0300

    message controller, service and middleware added, also changed express parameters

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticate } from "./middleware/ensureAuthenticate";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
 
+router.post(
+  "/messages",
+  ensureAuthenticate,
+  new CreateMessageController().handle
+);
 export { router };
