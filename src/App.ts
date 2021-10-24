6a7882395521627dde6bf6eba79cf84be37ef9fa
MatheusPatriota <mpatriota55@gmail.com>
2021-10-23 22:31:00 -0300

    cors configured

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,26 @@
 import express, { response } from "express";
 import "dotenv/config";
+import http from "http";
+import { Server } from "socket.io";
 import { router } from "./Routes";
+import cors from "cors";
 
 const app = express();
-app.use(express.json())
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection",socket => {
+  console.log(`connection established on socket ${socket.id}`)
+})
+
+app.use(express.json());
 
 app.use(router);
 
@@ -14,12 +31,12 @@ app.get("/github", (request, response) => {
   console.log(process.env.GITHUB_CLIENT_ID);
 });
 
-app.get("/signin/callback", (request, response) =>{
-  const {code} = request.query;
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
 
   return response.json(code);
-})
+});
 
-app.listen(4000, () => {
+serverHttp.listen(4000, () => {
   console.log("🚀 server is running on port 4000");
 });
